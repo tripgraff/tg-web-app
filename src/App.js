2b0137db2ba873b534dc34a809c9ebee30865481
tripgraff <tripgraffmac@Noutbuk-tripgraff.local>
2025-06-06 22:47:04 +0300

    3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useEffect } from 'react';
 import './App.css';
+import { useTelegram } from "./hooks/useTelegram";
 
 
 function App() {
+  const {onToggleButton, tg} = useTelegram(); 
 
   useEffect(() => {
     tg.ready();
@@ -10,7 +12,7 @@ function App() {
 
   return (
     <div className="App">
-      work
+      <button onClick={onToggleButton }>togle</button>
     </div>
   );
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,17 +1,15 @@
 import React from "react";
+import { useTelegram } from "../../hooks/useTelegram";
 
 const header = () => {
-    const tg = window.Telegram.WebApp
 
-    const onClose = () => {
-     tg.close()
-    }
+    const { user, onClose } = useTelegram();
 
     return (
         <div className={'header'}>
             <button onClick={onClose}>закрыть</button>
             <span className={'username '}>
-                {tg.initDataUnsafe?.user?.username}
+                { user?.username}
             </span>
         </div>
     );
