bd4b7e203dd70657d46aa46aefaad60c79d8141b
tripgraff <tripgraffmac@Noutbuk-tripgraff.local>
2025-06-29 17:40:09 +0300

    30

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -28,7 +28,7 @@ const Form = () => {
         tg.MainButton.setParams({
             text: 'Отправить данные'
         })
-    }, [])
+    }, [tg])
 
     useEffect(() => {
         if(!street || !country) {
