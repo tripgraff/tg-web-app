193dc9c52d771dac0586c2d1344e947eef140201
tripgraff <tripgraffmac@Noutbuk-tripgraff.local>
2025-07-03 01:54:03 +0300

    33

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import './ProductList.css';
+import ProductItem from "../ProductItem/ProductItem";
+import {useTelegram} from "../../hooks/useTelegram";
+
+const products = [
+    {id: '1', title: 'Футболка @tripgraff', price: 1900, description: 'черная'},
+    {id: '2', title: 'Футболка @tripgraff', price: 1900, description: 'белая'}
+]
+
+const getTotalPrice = (items) => {
+    return items.reduce((acc, item) => {
+        return acc += item.price
+    }, 0)
+}
 
 const ProductList = () => {
+    const [addedItems, setAddedItems] = useState([]);
+    const {tg} = useTelegram();
+    const onAdd = (product) => {
+        const alreadeAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
 
+        if (alreadeAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id)
+        } else {
+            newItems = [...addedItems, product];
+        }
+
+        setAddedItems(newItems)
+
+        if(newItems.length === 0) {
+            tg.mainButton.hide();
+        } else {
+            tg.mainButton.show();
+            tg.mainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            })
+        }
+    }
     return (
-        <div>
-            ProductList
-             
+        <div className={'list'}>
+             {products.map(item => (
+                <ProductItem
+                    product = {item}
+                    onAdd = {onAdd}
+                    className={'item'}
+                />
+             ))}
         </div>
     );
 };
