d67ecd3132438c9a97daecfd4bf12a03cc92a16c
tripgraff <tripgraffmac@Noutbuk-tripgraff.local>
2025-07-03 19:28:21 +0300

    37

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import './button.css';
 
-const button = (props) => {
+const Button = (props) => {
     return (
         <button {...props} className={'button ' + props.className}/>
     );
 };
 
-export default button;
+export default Button;
