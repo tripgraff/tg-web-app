e9958f8e1dff24d7a975443c272ce93e030a9bd4
tripgraff <tripgraffmac@Noutbuk-tripgraff.local>
2025-07-03 19:02:43 +0300

    36

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -9,7 +9,7 @@ const ProductItem = ({product, className, onAdd}) => {
     }
 
     return (
-        <div className={'product' + className}>
+        <div className={'product ' + className}>
             <div className={'img'}/>
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
