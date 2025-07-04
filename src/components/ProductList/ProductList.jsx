3f7123cb6b12281dcc47f22a2ab94e63f7a0e1ed
tripgraff <tripgraffmac@Noutbuk-tripgraff.local>
2025-07-04 19:35:33 +0300

    44

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -9,7 +9,7 @@ const products = [
     {id: '2', title: 'Футболка @tripgraff', price: 1900, description: 'белая'}
 ]
 
-const getTotalPrice = (items) => {
+const getTotalPrice = (items = []) => {
     return items.reduce((acc, item) => {
         return acc += item.price
     }, 0)
@@ -17,29 +17,29 @@ const getTotalPrice = (items) => {
 
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([]);
-    const {tg, qureyId} = useTelegram();
+    const {tg, queryId} = useTelegram();
 
-        const onSendData = useCallback(() => {
-            const data = {
-                products: addedItems,
-                totalPrice: getTotalPrice(addedItems),
-                queryId,
-            }
-            fetch('https://localhost:8000', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data)
-        }, [])
-    
-        useEffect(() => {
-            tg.onEvent('mainButtonClicked', onSendData)
-            return () => {
-                tg.offEvent('mainButtonClicked', onSendData)
-            }
-        }, [onSendData])
+    const onSendData = useCallback(() => {
+        const data = {
+            products: addedItems,
+            totalPrice: getTotalPrice(addedItems),
+            queryId,
+        }
+        fetch('http://85.119.146.179:8000/web-data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+    }, [addedItems])
 
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
 
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find(item => item.id === product.id);
