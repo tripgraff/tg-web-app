30e8d2317466b0a44f7664d62634dc8fa9c1dcd1
tripgraff <tripgraffmac@Noutbuk-tripgraff.local>
2025-07-05 00:58:13 +0300

    46

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -32,7 +32,7 @@ const ProductList = () => {
             },
             body: JSON.stringify(data)
         })
-    }, [addedItems])
+    }, [addedItems, queryId])
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
