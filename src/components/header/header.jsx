0ad17bc856662497b7e2488e43856ec3fa76cac3
tripgraff <tripgraffmac@Noutbuk-tripgraff.local>
2025-07-08 20:53:36 +0300

    55

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -20,9 +20,12 @@
 // export default header;
 
 import React from 'react';
+import { useTelegram } from '../../hooks/useTelegram';
 import './header.css';
 
 const header = ({ cartItems }) => {
+    const { user, onClose } = useTelegram();
+
     return (
         <div className="header">
             <img src="/logo.png" alt="Logo" className="logo" />
@@ -32,6 +35,8 @@ const header = ({ cartItems }) => {
                     <path d="M3 3H5L5.4 5H19L17 12H7L6 5H3V3ZM6 7H16.5L17.5 10H7.5L6 7ZM7 13C6.44772 13 6 13.4477 6 14C6 14.5523 6.44772 15 7 15C7.55228 15 8 14.5523 8 14C8 13.4477 7.55228 13 7 13ZM17 13C16.4477 13 16 13.4477 16 14C16 14.5523 16.4477 15 17 15C17.5523 15 18 14.5523 18 14C18 13.4477 17.5523 13 17 13Z" fill="var(--tg-theme-button-text-color)" />
                 </svg>
             </div>
+            <button onClick={onClose} className="close-button">Закрыть</button>
+            <span className="username">{user?.username || 'Гость'}</span>
         </div>
     );
 };
