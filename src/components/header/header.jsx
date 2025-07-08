3c7ae3a3eccf3a5f4871795500dc3ad405f6aa77
tripgraff <tripgraffmac@Noutbuk-tripgraff.local>
2025-07-08 23:11:48 +0300

    61

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -26,7 +26,7 @@ const header = ({ cartItems }) => {
     return (
         <div className="header">
             <a href='/'>          <svg className='logo' version="1.0" xmlns="http://www.w3.org/2000/svg"
-                width="60.000000pt" height="60.000000pt" viewBox="0 0 1700.000000 1700.000000"
+                width="70.000000pt" height="70.000000pt" viewBox="0 0 1700.000000 1700.000000"
                 preserveAspectRatio="xMidYMid meet">
 
                 <g transform="translate(0.000000,1700.000000) scale(0.100000,-0.100000)"
